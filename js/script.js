54d25a75000023f9f9b345f88979e1fa053b8e86
MarcoB <mrc.bsllt@gmail.com>
2021-01-13 12:24:12 +0100

    sistemato html prezzo di prodotti

--- js/script.js
+++ js/script.js
@@ -83,24 +83,24 @@ var app = new Vue(
         {
           name: "Brush",
           image: "brush_dark-800x800.webp",
-          price: "&#36;15.00"
+          price: "15.00"
         },
         {
           name: "Scissors",
           image: "scissors-600x600.webp",
-          price: "&#36;85.00"
+          price: "85.00"
 
         },
         {
           name: "Hot oil",
           image: "hot_oil_dark-400x400.webp",
-          price: "&#36;15.00"
+          price: "15.00"
 
         },
         {
           name: "Straight Razor",
           image: "straight_razor_dark-600x600.webp",
-          price: "&#36;30.00"
+          price: "30.00"
 
         },
       ],
